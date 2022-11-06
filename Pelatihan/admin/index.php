03662ccf3dd163b57893f44b6bf7f87b53dc9a74
Aldi Triavin <aldi3avin@gmail.com>
2022-11-06 17:43:04 +0700

    Update format penulisan
    update format penulisan file login dan index

--- Pelatihan/admin/index.php
+++ Pelatihan/admin/index.php
@@ -1,33 +1,33 @@
 <html>
-<head>
-<title>Membuat Login Dengan PHP dan MySQLi </title>
-</head>
-<body>
-<h2>Halaman Admin</h2>
-<br/>
-<!– cek apakah sudah login –>
-<?php
-session_start();
-if($_SESSION['status']!="login"){
-header("location:../login.php?pesan=belum_login");
-}
-?>
-<h4>Selamat datang, <?php echo $_SESSION['username']; ?>! anda telah login.</h4>
-<h4>Master Data</h4>
-<a href="dosen"><button>Data Dosen</button></a>||
-<a href="mahasiswa"><button>Data Mahasiswa</button></a>||
-<a href="matakuliah"><button>Data Matakuliah</button></a>||
-<a href="semester"><button>Data Semester</button></a>
-<br/>
-<h4>Transaksi</h4>
-<a href="jadwal"><button>BUAT JADWAL</button></a>||
+    <head>
+        <title>Membuat Login Dengan PHP dan MySQLi </title>
+    </head>
+    <body>
+        <h2>Halaman Admin</h2>
+        <br/>
+        <!– cek apakah sudah login –>
+        <?php
+        session_start();
+        if($_SESSION['status']!="login"){
+        header("location:../login.php?pesan=belum_login");
+        }
+        ?>
+        <h4>Selamat datang, <?php echo $_SESSION['username']; ?>! anda telah login.</h4>
+        <h4>Master Data</h4>
+        <a href="dosen"><button>Data Dosen</button></a>||
+        <a href="mahasiswa"><button>Data Mahasiswa</button></a>||
+        <a href="matakuliah"><button>Data Matakuliah</button></a>||
+        <a href="semester"><button>Data Semester</button></a>
+        <br/>
+        <h4>Transaksi</h4>
+        <a href="jadwal"><button>BUAT JADWAL</button></a>||
 
-<a href="krs"><button>KRS MAHASISWA</button></a>
+        <a href="krs"><button>KRS MAHASISWA</button></a>
 
-<h4>Pengaturan</h4>
-<a href="user"><button>Kelola Data User</button></a>
-<br/>
-<br/>
-<a href="logout.php">LOGOUT</a>
-</body>
+        <h4>Pengaturan</h4>
+        <a href="user"><button>Kelola Data User</button></a>
+        <br/>
+        <br/>
+        <a href="logout.php">LOGOUT</a>
+    </body>
 </html>
