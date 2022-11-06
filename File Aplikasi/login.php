0f5228af283ee75ac94ebf3784ec334fd0b607ac
Aldi Triavin <aldi3avin@gmail.com>
2022-11-06 17:10:33 +0700

    Update Login.php

--- File Aplikasi/login.php
+++ File Aplikasi/login.php
@@ -1,17 +1,3 @@
 <?php
-class koneksi
-{
-    public function get_koneksi()
-    {
-        $conn = mysqli_connect("localhost","root","","pelatihan_lsp");
-        if(mysqli_connect_errno()){
-            echo "Koneksi database gagal :" .mysqli_connect_error();
-        }
-        return $conn;
-    }
-}
 
-
-$konek = new koneksi();
-$koneksi = $konek->get_koneksi();
 ?>
